41fd0bf62f3d85ee7aff8debb045907718ba9df9
MOHAMMED TAHER <100471051+samhamed@users.noreply.github.com>
2023-11-16 20:24:12 +0100

    16/11/2023

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const cors = require("cors");
+const { readdirSync } = require('fs');
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("welcome from home");
+// ROUTES
+readdirSync("./routes").map((r) => {
+    app.use("/", require("./routes/" + r));
 });
-app.get("/books", (req, res) => {
-  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
+
+// database connection
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.on("open", () => console.log("connect to database"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
-app.listen(8000, () => {
-  console.log("server is lestining...");
-});
+
